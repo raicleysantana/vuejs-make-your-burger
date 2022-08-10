196b7262670386e07a501608159c70e0ac0eb65f
raicleysantana <raicleysantana1@gmail.com>
2022-08-09 23:47:00 -0400

    Aula dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="burger-table">
+        <div>
+            <div id="burger-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente</div>
+                <div>Pão:</div>
+                <div>Carne:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+
+            <div id="burger-table-rows">
+                <div class="burger-table-row">
+                    <div class="order-number">1</div>
+                    <div>Jõao</div>
+                    <div>Paõ de trigo</div>
+                    <div>Maminha</div>
+                    <div>
+                        <ul>
+                            <li>Salame</li>
+                            <li>Tomate</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <select name="status" id="status">
+                            <option value="">Selecione o status do hamburger</option>
+                        </select>
+                        <button></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped>
+</style>
